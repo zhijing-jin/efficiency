44ea371c69d5194b46ecc145d3e25360e63d188d
Jin-Zhijing <Fascinating@31-35-113.wireless.csail.mit.edu>
2018-06-27 10:25:53 -0400

    added first module

---
+++ efficiency/log.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from __future__ import division, print_function
+import pickle
+import random
+import numpy as np
+import sys
+import argparse
+import os.path
+import datetime
+
+
+def debug(expression):
+    frame = sys._getframe(1)
+    print(expression, ':', repr(eval(expression, frame.f_globals, frame.f_locals)))
+
+
+def write_var(var, path='data/debug/var'):
+    with open(path, 'w') as f:
+        f.write(path.split('/')[-1] +
+                ' = ' + repr(var) + '\n')
+
+
+def fwrite(new_doc, path):
+    with open(path, 'w') as f:
+        f.write(new_doc)
+
+
+def fread(path):
+    with open(path, 'r') as f:
+        return f.readlines()
+
+
+def show_time():
+    print('⏰ time:', datetime.datetime.now().strftime('%m%d%H%M-%S'))
+
+
+def bug():
+    import pdb
+    pdb.set_trace()
+    # you can use "c" for continue, "p variable", "p locals", "n" for next
+    # you can use "!a += 1" for changes of variables
+    # you can use "import code; code.interact(local=locals)" to iPython with all variables
