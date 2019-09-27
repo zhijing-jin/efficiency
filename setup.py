1bb674f6653891542edd7e7fcdb7469ec9c6745e
zhijing-jin <zhijing@mit.edu>
2019-09-27 14:28:19 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 from setuptools import setup, find_packages
 
+with open('README.md') as f:
+    long_description = f.read()
+
 setup(
     name='efficiency',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='A package for efficient programming',
-    long_description=open('README.md').read(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=['numpy'],
     url='https://github.com/zhijing-jin/efficiency',
     author='Z',
-    author_email='zhijing@mit.edu'
+    author_email='zhijing.jin@connect.hku.hk'
 )
