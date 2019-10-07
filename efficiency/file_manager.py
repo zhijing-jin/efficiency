a46b019eeaa438ce7783dfcb8e3ca641d2c9a97a
zhijing-jin <zhijing@mit.edu>
2019-10-07 13:20:56 +0800

    Update file_manager.py

--- efficiency/file_manager.py
+++ efficiency/file_manager.py
@@ -1,23 +1,22 @@
 from os.path import isdir, dirname, realpath, basename
 from os import listdir, path
-from itertools import chain
 from efficiency.function import shell
 from efficiency.log import show_var
 
 
 class FileManager():
 
-    def __init__(self, dir=dirname(realpath(__file__))):
-        self.files = self.recurse_files(dir)
+    def __init__(self, dir=dirname(realpath(__file__)), file_filter=lambda f: True):
+        self.files = self.recurse_files(dir, file_filter=file_filter)
 
     @staticmethod
-    def recurse_files(folder, filter=lambda f: True):
+    def recurse_files(folder, file_filter=lambda f: True):
         if isdir(folder):
             return [path.join(folder, f) for f in listdir(folder)
-                    if filter(f)]
+                    if file_filter(f)]
         return [folder]
 
-    def rename_files(self, prefix='My_mp3_'):
+    def rename_files(self, prefix='my_MP3_'):
         for f in self.files:
             dir = dirname(f)
             fname = basename(f)
@@ -27,7 +26,7 @@ class FileManager():
             show_var(['cmd'])
             shell(cmd)
 
-
+            
 if __name__ == '__main__':
     fm = FileManager()
-    fm.rename_files(prefix='My_mp3_')
+    fm.rename_files(prefix='my_MP3_')
