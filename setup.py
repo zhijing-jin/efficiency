62a6158559c5c69bd7f0f667493184e38a3d6a0f
Zhijing Jin <31386964+zhijing-jin@users.noreply.github.com>
2022-08-29 13:24:08 -0700

    added requirements

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open('README.md') as f:
 
 setup(
     name='efficiency',
-    version='0.4',
+    version='1.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='A package for efficient programming',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    install_requires=['spacy'],
+    install_requires=['spacy', 'nltk', 'pandas', 'numpy'],
     url='https://github.com/zhijing-jin/efficiency',
     author='Z',
     author_email='zhijing.jin@connect.hku.hk'
