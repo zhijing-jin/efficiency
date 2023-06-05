c63126e325458b51d531ee1af0b8eccd94a761ec
Zhijing Jin <31386964+zhijing-jin@users.noreply.github.com>
2023-06-05 17:28:38 +0200

    improve chatbot

--- efficiency/nlp.py
+++ efficiency/nlp.py
@@ -35,51 +35,145 @@ class NLP:
 
 
 class Translator:
-    def __init__(self, cache_file_pattern='.cache_{src}2{tgt}.json', load_translator=True):
-        self.cache_file_pattern = cache_file_pattern
-        from collections import defaultdict
-        self.cache = defaultdict(dict)
+    def __init__(self, cache_file='.cache_trans_lookup.csv', load_translator=True):
+        self.cache_file = cache_file
+        self.cache = self.load_cache()
         if load_translator:
             from googletrans import Translator
             self.translator = Translator()
 
     def load_cache(self):
+        from collections import defaultdict
+        cache = defaultdict(dict)
         from efficiency.log import fread
-        fread(self.cache_file)
+        data = fread(self.cache_file, verbose=False)
+        data = [i for i in data if (i['input'] != i['output']) and i['input'].strip() and i['output'].strip()]
+        for i in data:
+            cache[(i['src_lang'], i['tgt_lang'])][i['input']] = i['output']
+            cache[(i['tgt_lang'], i['src_lang'])][i['output']] = i['input']
 
-    def translate(self, text, src_lang='en', tgt_lang='de', verbose=True, enable_cache=False):
+        return cache
+
+    def save_cache(self, text, translated_text, src_lang, tgt_lang):
+        if text not in self.cache[(src_lang, tgt_lang)]:
+            datum = [{
+                'src_lang': src_lang,
+                'tgt_lang': tgt_lang,
+                'input': text,
+                'output': translated_text,
+            }]
+            self.cache[(src_lang, tgt_lang)][text] = translated_text
+            self.cache[(tgt_lang, src_lang)][translated_text] = text
+
+            from efficiency.log import write_dict_to_csv
+            write_dict_to_csv(datum, self.cache_file, mode='a')
+
+    def raw_translate(self, text, src_lang, tgt_lang):
+        def successful_pass():
+            translated_text = self.translator.translate(text, src=src_lang, dest=tgt_lang).text
+
+            if translated_text != text and translated_text.strip():
+                return translated_text
+
+        if_success = successful_pass()
+        if if_success:
+            return if_success
+        else:
+            import time
+            time.sleep(10)
+            if_success = successful_pass()
+            if if_success:
+                return if_success
+            else:
+                print(
+                    "[Error] Translation failed. You have very likely reached the limit of the `googletrans' "
+                    "library. Try to wait for a while or change your IP address. Alternatively, you can also edit the "
+                    "source code here to change it to Google cloud API by setting up your credentials.")
+                print(src_lang, tgt_lang, text)
+                import pdb;
+                pdb.set_trace()
+
+    def translate(self, text, src_lang='en', tgt_lang='de', verbose=True, enable_cache=True):
         if src_lang == tgt_lang:
             return text
 
-        if enable_cache:
-            this_cache_file = self.cache_file_pattern.format(src=src_lang, tgt=tgt_lang)
-            lang_pair = tuple((src_lang, tgt_lang))
-            if lang_pair not in self.cache:
-                import os
-                if os.path.exists(this_cache_file):
-                    from efficiency.log import fread
-                    self.cache[lang_pair] = fread(this_cache_file)
-
-            if text in self.cache[lang_pair]:
-                translated_text = self.cache[lang_pair][text]
-            else:
-                translated_text = self.translator.translate(text, src=src_lang, dest=tgt_lang).text
-                self.cache[lang_pair][text] = translated_text
-                from efficiency.log import fwrite
-                fwrite(self.cache[lang_pair], this_cache_file)
+        this_cache = self.cache[(src_lang, tgt_lang)]
+        if enable_cache and (text in this_cache):
+            translated_text = this_cache[text]
         else:
-            translated_text = self.translator.translate(text, src=src_lang, dest=tgt_lang).text
+            translated_text = self.raw_translate(text, src_lang, tgt_lang)
+            self.save_cache(text, translated_text, src_lang, tgt_lang)
 
-        if translated_text == text:
-            print("[Warn] Translation is failed. You have very likely reached the limit of the `googletrans' library. "
-                  "Try to wait for a while or change your IP address. Alternatively, you can also edit the source "
-                  "code here to change it to Google cloud API by setting up your credentials.")
-            import pdb;
-            pdb.set_trace()
         if verbose:
             from efficiency.log import show_var
             show_var(['text', 'translated_text', ])
         return translated_text
+
+
+    @staticmethod
+    def print_countries(langs):
+        langs = sorted(langs)
+
+        import pycountry
+        rows = [
+            {'lang': lang,
+             'country': pycountry.languages.get(alpha_2=lang).name if pycountry.languages.get(alpha_2=lang) else None
+             }
+            for lang in langs
+        ]
+        import pandas as pd
+        df = pd.DataFrame(rows, index=None)
+        print(df)
+        import pdb;
+        pdb.set_trace()
+        return df
+
+
+    @staticmethod
+    def get_language_list(list_from=['googletrans', 'pycountry', 'langcodes'][0]):
+        langs = []
+        if list_from == 'googletrans':
+            from googletrans import LANGUAGES
+            translateable_langs = []
+            for language_code, language_name in LANGUAGES.items():
+                translateable_langs.append(language_code)
+            translateable_langs = sorted(translateable_langs)
+            langs = translateable_langs
+        elif list_from == 'pycountry':
+            import pycountry
+            code2family = [{"alpha_3": i.alpha_3, "name": i.name} for i in pycountry.language_families]
+
+            pyc_langs = []
+            for language in pycountry.languages:
+                name = language.name.replace(' (macrolanguage)', '').strip()
+                name = name.split('(', 1)[0].strip()
+                item = {'name': name}
+                try:
+                    item['lang'] = language.alpha_2
+                except:
+                    try:
+                        item['lang'] = language.alpha_3
+                    except:
+                        pass
+                pyc_langs.append(item)
+            additional_dict = [
+                {'lang': 'zh-cn', 'name': 'Chinese (Simplified)'},
+                {'lang': 'zh-tw', 'name': 'Chinese (Traditional)'},
+                {'lang': 'iw', 'name': 'Modern Hebrew'},
+                {'lang': 'jw', 'name': 'Javanese'},
+                {'lang': 'me', 'name': 'Montenegrin'},
+            ]
+            pyc_langs.extend(additional_dict)
+            from langcodes import Language
+            for item in pyc_langs:
+                language_code = item['lang']
+                # lang = Language.get(language_code)
+                # item['family'] = lang.family_name()
+            import pandas as pd
+            pyc_langs = pd.DataFrame(pyc_langs)
+            langs = pyc_langs
+
+        return langs
 
 
 class Chatbot:
@@ -95,7 +189,6 @@ class Chatbot:
         'gpt3.03': "curie",
         'gpt3.02': "babbage",
         'gpt3.01': "ada",
-
     }
     engine2pricing = {
         "gpt-3.5-turbo": 0.002,
@@ -110,20 +203,24 @@ class Chatbot:
         "ada": 0.0004,
     }
 
-    def __init__(self, model_version='gpt3.5', max_tokens=100, output_file='.cache_gpt_responses.csv',
-                 system_prompt="You are a helpful assistant.",
-                 openai_key_alias='OPENAI_API_KEY', ):
+    def __init__(self, model_version='gpt3.5', max_tokens=100, output_file=None, output_folder='./',
+                 system_prompt="You are a helpful assistant.", cache_files=[],
+                 openai_key_alias='OPENAI_API_KEY', openai_org_alias='OPENAI_ORG_ID', ):
         import os
-        openai_api_key = os.environ[openai_key_alias]
+        import openai
+        api_key = os.environ[openai_key_alias]
+        openai.api_key = api_key
+        if openai_org_alias != 'OPENAI_ORG_ID':
+            organization_id = os.environ[openai_org_alias]
+            openai.organization = organization_id
 
         self.model_version = model_version
+        self.engine = self.model_version2engine.get(model_version, model_version)
         self.max_tokens = max_tokens
         self.system_prompt = system_prompt
-        self.output_file = output_file
-        self.gpt_files = [output_file]
+        self.output_file = f'{output_folder}/.cache_{model_version}.csv' if output_file is None else output_file
+        self.gpt_files = cache_files + [output_file]
 
-        import openai
-        openai.api_key = openai_api_key
         self.openai = openai
         self.num_tokens = []
         self.cache = self.load_cache()
@@ -165,13 +262,14 @@ class Chatbot:
 
     @property
     def _total_cost(self):
-        return sum(self.num_tokens) // 1000 * self.engine2pricing[self.engine]
+        default_price = self.engine2pricing['davinci']
+        return sum(self.num_tokens) // 1000 * self.engine2pricing.get(self.engine, default_price)
 
     def print_cost(self):
         print(f"[Info] Spent ${self._total_cost} for {sum(self.num_tokens)} tokens.")
 
     def save_cache(self, question, response_text):
-        if not (question in self.cache):
+        if (not (question in self.cache)) and response_text:
             self.cache[question] = response_text
             datum = [{
                 'pred': response_text,
@@ -188,9 +286,23 @@ class Chatbot:
             cache.update({i[f'query{q_i}']: i[f'pred{q_i}'] for i in data
                           for q_i in list(range(10)) + ['']
                           if f'query{q_i}' in i})
+        cache = {k: v for k, v in cache.items() if v}  # there are cases where the response is empty
         return cache
 
     def ask(self, *args, **kwargs):
+        def repeat():
+            sec = 100
+            print(f'[Info] openai.error.RateLimitError. Wait for {sec} seconds')
+            self.print_cost()
+            '''
+            Default rate limits for gpt-4/gpt-4-0314 are 40k TPM and 200 RPM. Default rate limits for gpt-4-32k/gpt-4-32k-0314 are 80k PRM and 400 RPM. 
+            https://platform.openai.com/docs/guides/rate-limits/overview
+            '''
+
+            import time
+            time.sleep(sec)
+            return self.ask(*args, **kwargs)
+
         import openai
         try:
             return self.raw_query(*args, **kwargs)
@@ -204,33 +316,15 @@ class Chatbot:
                 if turn['role'] == 'assistant':
                     turn['content'] = turn['content'][:1000]
 
-        except openai.error.RateLimitError or openai.error.APIError:
-            sec = 100
-            print(f'[Info] openai.error.RateLimitError. Wait for {sec} seconds')
-            self.print_cost()
-            '''
-            Default rate limits for gpt-4/gpt-4-0314 are 40k TPM and 200 RPM. Default rate limits for gpt-4-32k/gpt-4-32k-0314 are 80k PRM and 400 RPM. 
-            https://platform.openai.com/docs/guides/rate-limits/overview
-            '''
-
-            import time
-            time.sleep(sec)
-            return self.ask(*args, **kwargs)
-        except:
-            sec = 100
-            print(f'[Info] Unknown error. Wait for {sec} seconds')
-            self.print_cost()
-            '''
-            Default rate limits for gpt-4/gpt-4-0314 are 40k TPM and 200 RPM. Default rate limits for gpt-4-32k/gpt-4-32k-0314 are 80k PRM and 400 RPM. 
-            https://platform.openai.com/docs/guides/rate-limits/overview
-            '''
-
-            import time
-            time.sleep(sec)
-            return self.ask(*args, **kwargs)
+        except openai.error.RateLimitError:
+            return repeat()
+        except openai.error.APIError:
+            return repeat()
+        # except:
+        #     repeat()
 
     def raw_query(self, question,
-                  turn_off_cache=False,
+                  turn_off_cache=False, valid_ways=['cache', 'api_call'],
                   continued_questions=False,
                   sentence_completion_mode=False,
                   max_tokens=None, stop_sign="\nQ: ",
@@ -239,17 +333,18 @@ class Chatbot:
                   enable_pdb=False, verbose=True, only_response=True, disable_system_role=False,
                   save_cache_with_system_role=False,
                   ):
+        enable_api = 'api_call' in valid_ways
         if model_version is not None:
-            engine = self.model_version2engine[model_version]
+            engine = self.model_version2engine.get(model_version, model_version)
         elif engine is not None:
             engine = engine
         else:
-            engine = self.model_version2engine[self.model_version]
+            engine = self.engine
+        self.engine = engine  # to be called in print_cost()
 
         max_tokens = self.max_tokens if max_tokens is None else max_tokens
         verbose = True if enable_pdb else verbose
 
-        self.engine = engine
         if_newer_engine = engine.startswith('gpt-3.5') or engine.startswith('gpt-4')
 
         if not continued_questions:
@@ -273,9 +368,11 @@ class Chatbot:
             pdb.set_trace()
         if (cache_input in self.cache) & (not turn_off_cache):
             response_text = self.cache[cache_input]
-            if not if_newer_engine: response_text = response_text.split(stop_sign, 1)[0]
-            if verbose: print(f'[Info] Using cache for {cache_input}')
-        else:
+            if not if_newer_engine:
+                response_text = str(response_text).split(stop_sign, 1)[0]
+            response_text = response_text.strip()
+            if verbose: print(f'[Info] Using cache for "{cache_input[:10]}..."')
+        elif enable_api:
             openai = self.openai
             if if_newer_engine:
                 response = openai.ChatCompletion.create(
@@ -298,6 +395,8 @@ class Chatbot:
             self.num_tokens.append(response['usage']["total_tokens"])
             response_text = response_text.strip()
             if verbose: self.print_cost()
+        else:
+            response_text = ''
 
         if if_newer_engine:
             output = f"S: {self.dialog_history[0]['content']}\n\n" \
@@ -315,7 +414,7 @@ class Chatbot:
             import pdb;
             pdb.set_trace()
 
-        self.save_cache(cache_input, response_text)
+        if enable_api: self.save_cache(cache_input, response_text)
 
         if only_response:
             return response_text
