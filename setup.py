3b84653e3d1ac74b54f0c3c5e48a0ffc7878bb7f
Zhijing Jin <31386964+zhijing-jin@users.noreply.github.com>
2023-12-26 18:13:22 +0100

    added version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='efficiency',
-    version='1.31',
+    version='2.0',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='A package for efficient programming',
