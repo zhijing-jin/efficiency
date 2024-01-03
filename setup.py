69d191bc1009be5d4f1320388367ff6a0733433b
Zhijing Jin <31386964+zhijing-jin@users.noreply.github.com>
2024-01-03 22:08:42 +0100

    Added openAI previous version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     description='A package for efficient programming',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    install_requires=['pandas', 'numpy'],
+    install_requires=['pandas', 'numpy', 'openai==0.28'],
     url='https://github.com/zhijing-jin/efficiency',
     author='Zhijing Jin'
 )
